eb9b0a215b60dd31d82f7f981be04a204fe91346
olus2000 <alsabak@gmail.com>
2023-04-22 16:10:54 +0200

    Basic upload endpoint

--- api/app/api.py
+++ api/app/api.py
@@ -1,13 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request, current_app, abort
 from flask.json import jsonify
+
+import os.path
+
+from app.db.populate import populate
+from app.db.create import create
 
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 
-@bp.route('/upload')
+@bp.route('/upload', methods=('POST',))
 def upload():
-    return "Mock upload success"
+    if 'data' not in request.files:
+        abort(400)
+    db_path = os.path.join(current_app.instance_path, 'test.db')
+    if not os.path.exists(db_path):
+        create(db_path)
+    file_path = os.path.join(current_app.instance_path, 'data.csv')
+    request.files['data'].save(file_path)
+    populate(db_path, file_path)
+    return "Ok"
 
 
 @bp.route('/download')
