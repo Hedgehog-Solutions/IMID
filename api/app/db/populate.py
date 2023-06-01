452f11c24060d26de077bab6cadc0e2214e03854
tswiders <01167294@pw.edu.pl>
2023-06-01 17:50:25 +0200

    Przejście z SQLite na PosgreSQL

--- api/app/db/populate.py
+++ api/app/db/populate.py
@@ -1,53 +1,77 @@
 import sqlite3
+import psycopg2
 import pandas as pd
 
 
-def populate(db_name, data_name):
+def populate(host, database, user, password, data_name):
 
     df = pd.read_csv(data_name, sep="\t", index_col=0)
     df = df.rename(columns=lambda x: x.lower())
 
     # utworzenie połączenia z bazą danych
-    conn = sqlite3.connect(db_name)
+    conn = psycopg2.connect(
+        host=host,
+        database=database,
+        user=user,
+        password=password)
+    
+    cursor = conn.cursor()
 
     # wczytanie wszystkich stałych atrybutów variants
     variants_atr = list()
-    cursor = conn.execute("PRAGMA table_info(variants)")
+    cursor.execute("""SELECT column_name
+                    FROM information_schema.columns
+                    WHERE table_name = 'variants'""")
     for row in cursor:
-        variants_atr.append(row[1])
+        variants_atr.append(row[0])
 
     # wczytanie wszystkich aktualnych metadanych z variant_columns
     variants_metadata = list()
-    cursor = conn.execute("SELECT metadata FROM variant_columns")
+    cursor.execute("SELECT metadata FROM variant_columns")
     for row in cursor:
         variants_metadata.append(row)
-        
-    conn.execute("INSERT INTO versions (version_id, from_date) VALUES (1, '14.04.2023')",)
 
+    cursor.execute("INSERT INTO versions (version_id, from_date) VALUES (1, '14.04.2023')",)
 
+    i = 0 #TODO TEMP
     for index, row in df.iterrows():
-        conn.execute(f'''INSERT INTO variants (sex, chr, pos, ref, alt) 
-                         VALUES ("{row['sex']}", "{row['chr']}", "{row['pos']}", "{row['ref']}", "{row['alt']}")''')
-        variant_id = conn.execute("SELECT last_insert_rowid()").fetchone()[0]
-        
+        cursor.execute('''INSERT INTO variants (sex, chr, pos, ref, alt) 
+                    VALUES (%s, %s, %s, %s, %s) RETURNING variant_id''', (row['sex'], row['chr'], row['pos'], row['ref'], row['alt']))
+
+        variant_id = cursor.fetchone()[0]
+        print(variant_id) #TODO TEMP
+
+        values = []
         for column in df.columns:
             if column not in variants_atr:
                 if column not in variants_metadata:
-                    conn.execute(f'''INSERT INTO variant_columns (metadata, type)
-                                 VALUES ("{column}", "object")''')
+                    cursor.execute('''INSERT INTO variant_columns (metadata, type)
+                                 VALUES (%s, %s)''', (column, "object"))
                     variants_metadata.append(column)
                 
-                column_id = conn.execute(f"SELECT column_id FROM variant_columns WHERE metadata = '{column}'").fetchone()[0]
+                cursor.execute("SELECT column_id FROM variant_columns WHERE metadata = %s",(column, ))
+                column_id = cursor.fetchone()[0]
                 
-                conn.execute(f'''INSERT INTO variant_column_values (variant_id, column_id, version_id, value)
-                                 VALUES ({variant_id}, {column_id}, 1, "{row[column]}")''')
-            
+                val = (variant_id, column_id, 1, row[column])
+                values.append(val)
+
+        cursor.executemany('''INSERT INTO variant_column_values (variant_id, column_id, version_id, value)
+                              VALUES (%s, %s, %s, %s)''', values)
+                
+        #TODO TEMP
+        i = i+1
+        if i > 50:
+            break
+
     print("Database populated successfully")
             
     conn.commit()
 
+    cursor.close()
     conn.close()
 
 
-if __name__ == '__main__':
-    populate('test.db', 'RG-corriell_S7_VEP.tsv')
+# if __name__ == '__main__':
+
+#     file_path = "RG-corriell_S7_VEP.tsv"
+#     populate(host, database, user, password, file_path)
